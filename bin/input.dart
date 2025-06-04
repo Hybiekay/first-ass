f34fef9316f28a7fd3e9db356acefc940c060ea1
unknown <hybiekay2@gmail.com>
2025-06-04 14:42:35 +0100

    Addin bin and first commit

---
+++ bin/input.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+void main() {
+  // print("enter you name");
+  // String? name = stdin.readLineSync();
+  // print("Hello $name");
+
+  // print("Enter a number");
+  // String? num1 = stdin.readLineSync();
+
+  // print("Enter the second number");
+
+  // String? num2 = stdin.readLineSync();
+
+  // int convertedNum1 = int.parse("$num1");
+  // int convertedNum2 = int.parse("$num2");
+  // int sum = convertedNum1 + convertedNum2;
+  // print(
+  //   'the sum of $convertedNum1 and $convertedNum2 is ${convertedNum1 + convertedNum2}',
+  // );
+
+  // int currentYear = 2025;
+
+  // print("Enter Your birth year");
+  // String? birthYear = stdin.readLineSync();
+  // int convertedBirthYear = int.parse("$birthYear");
+  // int age = currentYear - convertedBirthYear;
+
+  // print("You are $age years old");
+}
